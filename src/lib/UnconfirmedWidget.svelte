ff0e5bbb666a32365035c3e959f83b59fb8ffa8a
c8e4 <c8e4@proton.me>
2023-10-11 14:57:56 +0200

    improve style

--- src/lib/UnconfirmedWidget.svelte
+++ src/lib/UnconfirmedWidget.svelte
@@ -7,8 +7,8 @@
     export let sale:UnconfirmedSale
 </script>
 
-<div class="box w-full flex flex-col">
-    <div class="w-full flex flex-col bg-black">
+<div class="p-4 box w-full flex flex-wrap backdrop-blur-md bg-opacity-10 shadow-sm">
+    <div class="grow flex flex-col justify-center">
 		{#each sale.tokens as token}
             <div class="flex gap-2">
                 <div class="font-mono">{token.tokenId.substr(0, 3)}...{token.tokenId.substr(
@@ -21,7 +21,8 @@
             </div>
         {/each}
 	</div>
-    <div class="w-full flex bg-red-900">
+    <div class="">
+        <div>confirming...</div>
         <a target="_blank" href="https://explorer.ergoplatform.com/en/transactions/{sale.transactionId}" class="underline">
             Transaction ID
         </a>
@@ -29,7 +30,11 @@
 </div>
 
 <style>
-	.box {
-		max-width: 600px;
-	}
+    .box{
+        max-width: 600px;
+        background: rgba(16, 16, 16, 0.0);
+        border: 1px solid #80808045;
+        border-top: 2px solid #a9a9a945;
+        color: rgb(144, 144, 144);
+    }
 </style>
